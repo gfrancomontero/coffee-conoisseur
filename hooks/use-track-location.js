060bf9696ecf93fa0221674b038831f16f403c03
Gonzalo Franco <gfrancomontero@gmail.com>
2022-08-21 22:01:09 +0200

    v1

---
+++ hooks/use-track-location.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const useTrackLocation = () => {
+  const [locationErrorMsg, setLocationErrorMsg] = useState("");
+  const [latLong, setLatLong] = useState("");
+  const [isFindingLocation, setIsFindingLocation] = useState(false);
+
+  const success = (position) => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    setIsFindingLocation(false);
+    setLatLong(`${latitude},${longitude}`);
+    setLocationErrorMsg("");
+  };
+
+  const error = () => {
+    setLocationErrorMsg("Error retrieving your location");
+    setIsFindingLocation(false);
+  };
+
+  const handleTrackLocation = () => {
+    setIsFindingLocation(true);
+    if (!navigator.geolocation) {
+      setLocationErrorMsg("Geolocation is not supported by your browser");
+    } else {
+      // status.textContent = "Locating…";
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  };
+
+  return {
+    latLong,
+    locationErrorMsg,
+    handleTrackLocation,
+    isFindingLocation,
+  };
+};
+
+export default useTrackLocation;
--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,101 +1,103 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
-import coffeeShopListJson from "../../data/coffee-stores.json";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../../styles/coffee-store.module.css";
+
 import cls from "classnames";
-import { fetchCoffeeStores } from '../../lib/coffee-stores'
-import { fetchIndividualCoffeeStore } from '../../lib/coffee-stores'
-import { fetchImageForFoursquareId } from '../../lib/coffee-stores'
 
-// Gonzalo already explained getStaticProps function in index.js
+import styles from "../../styles/coffee-store.module.css";
+
+import { fetchCoffeeStores } from "../../lib/coffee-stores";
+
 export async function getStaticProps(staticProps) {
+  const params = staticProps.params;
 
-  const results = await fetchIndividualCoffeeStore(staticProps.params.id);
-  const imgUrl = await fetchImageForFoursquareId(staticProps.params.id, 1);
-
+  const coffeeStores = await fetchCoffeeStores();
   return {
     props: {
-      coffeeStores: results,
-      image: imgUrl,
+      coffeeStore: coffeeStores.find((coffeeStore) => {
+        return coffeeStore.fsq_id.toString() === params.id; //dynamic id
+      }),
     },
-  }
+  };
 }
 
 export async function getStaticPaths() {
-  const coffeeStores = await fetchCoffeeStores(1);
+  const coffeeStores = await fetchCoffeeStores();
   const paths = coffeeStores.map((coffeeStore) => {
     return {
       params: {
-        id: coffeeStore.fsq_id.toString()
-      }
-    }
+        id: coffeeStore.fsq_id.toString(),
+      },
+    };
   });
-
   return {
-    paths: paths,
-    fallback: true
-  }
+    paths,
+    fallback: true,
+  };
 }
 
 const CoffeeStore = (props) => {
-  // we're incluidng this router.isFallback for when the fallback is true and there is no
-  // pre-rendered route matching the one requested in the url
   const router = useRouter();
   if (router.isFallback) {
-    return (
-      <div>Loading...</div>
-    )
+    return <div>Loading...</div>;
   }
-  // end of comment
+  const { name, address, neighbourhood, imgUrl } = props.coffeeStore;
 
-  const handleUpVoteButton = () => {
-    console.log("handle upvote button");
-  };
+  const handleUpvoteButton = () => {};
 
-  const { name, location } = props.coffeeStores;
-  const imgUrl = props.image;
-
-  return(
-    <div className={styles.container}>
+  return (
+    <div className={styles.layout}>
       <Head>
         <title>{name}</title>
       </Head>
-      <div className={styles.col1}>
-        <div className={styles.backToHomeLink}>
-          <Link href="/">
-            <a>Back home</a>
-          </Link>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">
+              <a>← Back to home</a>
+            </Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image
+            src={
+              imgUrl ||
+              "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+            }
+            width={600}
+            height={360}
+            className={styles.storeImg}
+            alt={name}
+          />
         </div>
-        <div className={styles.nameWrapper}>
-          <h1 className={styles.name}>{name}</h1>
+
+        <div className={cls("glass", styles.col2)}>
+          {address && (
+            <div className={styles.iconWrapper}>
+              <Image src="/static/icons/places.svg" width="24" height="24" />
+              <p className={styles.text}>{address}</p>
+            </div>
+          )}
+          {neighbourhood && (
+            <div className={styles.iconWrapper}>
+              <Image src="/static/icons/nearMe.svg" width="24" height="24" />
+              <p className={styles.text}>{neighbourhood}</p>
+            </div>
+          )}
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/star.svg" width="24" height="24" />
+            <p className={styles.text}>1</p>
+          </div>
+
+          <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
+            Up vote!
+          </button>
         </div>
-        <Image
-          src={imgUrl || "https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80" }
-          height={600}
-          width={360}
-          className={styles.storeImg}
-          alt={name}
-        ></Image>
-      </div>
-      <div className={cls("glass", styles.col2)}>
-        <div className={styles.iconWrapper}>
-          <Image src={'/static/icons/places.svg'} height={24} width={24} />
-          <p className={styles.text}>{location.address}</p>
-        </div>
-        <div className={styles.iconWrapper}>
-          <Image src={'/static/icons/nearMe.svg'} height={24} width={24} />
-          <p className={styles.text}>{location.neighborhood || location.cross_street || location.locality }</p>
-        </div>
-        <div className={styles.iconWrapper}>
-          <Image src={'/static/icons/star.svg'} height={24} width={24} />
-          <p className={styles.text}>1</p>
-        </div>
-        <button className={styles.upVoteButton} onClick={handleUpVoteButton}>UpVote!</button>
-        <br />
       </div>
     </div>
-  )
-}
+  );
+};
+
 export default CoffeeStore;
